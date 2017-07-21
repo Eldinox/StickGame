a87854440abbe5afb36b567f7588693d0ecaf095
Eldinox <waldemar.g94@web.de>
2017-07-21 20:09:41 +0200

    Images funktionieren, überschreiben sich aber noch

--- StickGame/src/Button.java
+++ StickGame/src/Button.java
@@ -1,81 +1,154 @@
 import java.awt.Color;
+import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 @SuppressWarnings("serial")
 public class Button extends JButton implements MouseListener
 {
 	int indexInt;
-	 String kante;
+	String kante;
+	
 	public Button()
 	{
+		setBackground(Color.WHITE);
 		addMouseListener(this);
 		addActionListener(e ->{    
 		JButton button = (JButton)e.getSource();
         String indexString = button.getText();
         indexInt = Integer.parseInt(indexString);
-        System.out.println("Index : "+indexInt);
-	});
+        System.out.println("Index : " + indexInt);
+		});
 	}
-
 
 	@Override
 	public void mouseClicked(MouseEvent clickE) 
 	{
-		//System.out.println("X: " + clickE.getX());
-		//System.out.println(" Y: " + clickE.getY());
 		if(clickE.getX() < 20 && clickE.getY() > 20 && clickE.getY() < 72)
 		{
 			System.out.println("Linke Kante");
-			this.setBackground(Color.RED);
-			
-			//View.changeStatus(this);
+			try 
+			{
+				Image links = ImageIO.read(getClass().getResource("LinkeKanteP1.png"));
+				this.setIcon(new ImageIcon(links));
+				checkNext(indexInt, "links");
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
+					
 		}
 		else if(clickE.getX() > 20 && clickE.getX() < 79 && clickE.getY() < 20)
 		{
 			System.out.println("Obere Kante");
-			this.setBackground(Color.BLUE);
+			try 
+			{
+				Image oben = ImageIO.read(getClass().getResource("ObereKanteP1.png"));
+				this.setIcon(new ImageIcon(oben));
+				checkNext(indexInt, "oben");
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
 		}
 		else if(clickE.getX() > 76 && clickE.getY() > 20 && clickE.getY() < 62)
 		{
 			System.out.println("Rechte Kante");
-			this.setBackground(Color.GREEN);
+			try 
+			{
+				Image rechts = ImageIO.read(getClass().getResource("RechteKanteP1.png"));
+				this.setIcon(new ImageIcon(rechts));
+				checkNext(indexInt, "rechts");
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
 		}
 		else if(clickE.getX() > 20 && clickE.getX() < 79 && clickE.getY() > 72)
 		{
 			System.out.println("Untere Kante");
-			this.setBackground(Color.YELLOW);
+			try 
+			{
+				Image unten = ImageIO.read(getClass().getResource("UntereKanteP1.png"));
+				this.setIcon(new ImageIcon(unten));
+				checkNext(indexInt, "unten");
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
 		}
 	}
-
-
 	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	public void mouseEntered(MouseEvent e) {}
 	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	public void mouseExited(MouseEvent e) {}
 	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	public void mousePressed(MouseEvent e) {}
 	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+	public void mouseReleased(MouseEvent e) {}
+	
+	private void checkNext(int indexInt, String side)
+	{
+		if(side == "links")
+		{
+			try 
+			{
+				Image img = ImageIO.read(getClass().getResource("RechteKanteP1.png"));
+				View.buttons[indexInt - 1].setIcon(new ImageIcon(img));
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}	
+		}
+		else if(side == "oben")
+		{
+			try 
+			{
+				Image img = ImageIO.read(getClass().getResource("UntereKanteP1.png"));
+				View.buttons[indexInt - 6].setIcon(new ImageIcon(img));
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}	
+		}
+		else if(side == "rechts")
+		{
+			try 
+			{
+				Image img = ImageIO.read(getClass().getResource("LinkeKanteP1.png"));
+				View.buttons[indexInt + 1].setIcon(new ImageIcon(img));
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}	
+		}
+		else if(side == "unten")
+		{
+			try 
+			{
+				Image img = ImageIO.read(getClass().getResource("ObereKanteP1.png"));
+				View.buttons[indexInt + 6].setIcon(new ImageIcon(img));
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}	
+		}
 	}
 	
-
 	public int getIndex()
 	{
 		return indexInt;
--- StickGame/src/View.java
+++ StickGame/src/View.java
@@ -1,7 +1,15 @@
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -11,7 +19,7 @@ public class View extends JFrame
 	static final int felderZahl = 36;
 	JFrame frame = new JFrame("Stickgame");
 	JPanel panel = new JPanel();
-	Button[] buttons = new Button[felderZahl];
+	public static Button[] buttons = new Button[felderZahl];
 	int[] disabledButtons = new int[]{0, 1, 4, 5, 6, 11, 24, 29, 30, 31, 34, 35};
 	int index = 0;
 	
@@ -19,14 +27,39 @@ public class View extends JFrame
 	{
 		frame.add(panel);
 		panel.setLayout(new GridLayout((int) Math.sqrt(felderZahl), (int) Math.sqrt(felderZahl)));
+		File path = new File("D:\\Github\\StickGame\\StickGame\\src");
 		for(int i = 0; i < buttons.length; i++)
 		{
 			buttons[i] = new Button();
+			//buttons[i].setBorder(BorderFactory.createEmptyBorder());
 			buttons[i].setText(Integer.toString(i));
 			buttons[i].setForeground(new Color(255, 255, 255, 0));
+			
+			
+			/* Das ist der Code damit die Bilder später überlappen und nicht überschrieben werden
+			 * try 
+			{
+				//BufferedImage imgAktuel = ImageIO.read(new File(path, "newImage.png"));
+				BufferedImage imgAktuel = buttons[i].getIcon();
+				BufferedImage imgOverlay = ImageIO.read(new File(path, "UntereKanteP1.png"));
+				int w = imgAktuel.getWidth();
+				int h = imgAktuel.getHeight();
+				BufferedImage newImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+				Graphics g = newImage.getGraphics();
+				g.drawImage(imgOverlay, 0, 0, null);
+				g.drawImage(imgAktuel, 0, 0, null);
+					
+				ImageIO.write(newImage, "PNG", new File(path, "newImage.png"));
+				Image img = ImageIO.read(getClass().getResource("newImage.png"));
+				buttons[i].setIcon(new ImageIcon(img));
+			}
+			catch(IOException e) 
+			{
+				System.out.println("fehler");
+			}*/
+			
 			if(disabledButtons[index] == i)
 			{
-				//System.out.println("ouafhgsoüuaf");
 				buttons[i].setEnabled(false);
 				buttons[i].setBackground(Color.BLACK);
 				buttons[i].setForeground(Color.BLACK);
@@ -35,27 +68,10 @@ public class View extends JFrame
 			panel.add(buttons[i]);
 		}
 		
-		
-		
 		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		frame.setSize(600, 600);
+		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
-	
-	/*public static void changeStatus(Button button)
-	{
-		System.out.print("ja");
-		
-		for(int i = 0; i < buttons.length; i++)
-		{
-			if(button == buttons[i])
-			{
-				System.out.print(i);
-			}
-		}
-	}*/
-	
-
-	
 }
